bcec60b290cb57836a7231c858e7730a7e760212
ucu62 <trufan62@gmail.com>
2022-02-15 21:26:34 +0200

    proiect initial

---
+++ selenim automation/proiect intelj v1/src/test/java/Autocomplete.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Autocomplete {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\sda 1\\chromedriver.exe");// cheia pentru intiarea in drevier
+        // i-am spus sistemului unde sa gsesca driver-ul
+        WebDriver driver=new ChromeDriver();// aici am creat un nou obiect al clasei web driver
+        /* o varabila este o adresa de memorie la care sunt stocte informati
+        o fuctie reprezinta un set de istrutiuni grupate sub unn singur nume
+        o clasa reprezinta un tpar care defineste arributele pe care le aibe un obiect si actiunile pe care trebuie sa le faca
+        un obiect este o instanta aunei clase adica oreprezentare reala creste pe baza tiparului definit in clasa
+        putem spun eca o functi erturneza ceva trimite rezultaul pe care il face ctre exterioe
+         */
+    }
+}
